7de1dc8ab737118c176f9acb656a1a89274d6d70
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-10-22 06:21:18 +0700

    build main section

--- src/components/mainSection/MainSection.jsx
+++ src/components/mainSection/MainSection.jsx
@@ -4,7 +4,9 @@ export const MainSection = () => {
       <div className="flex items-center justify-between">
         <h1>The Best Movie Recomendation & the Latest Movies from us</h1>
         <h3>
-          Enjoy Various films that we have recommended for you and your familly
+          Enjoy Various films that we have
+          <br />
+          recommended for you and your familly
         </h3>
       </div>
     </div>
