d6b7b7b85f7052fdb22379aa328fbd4415a7e097
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-10-21 17:18:34 +0700

    build section hero

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,5 +1,8 @@
 // IMPORT IMAGE
 import hero from "../assets/hero.jpg";
+import scene1 from "../assets/scene1.jpg";
+import scene2 from "../assets/scene2.jpg";
+import scene3 from "../assets/scene3.jpg";
 
 export const NavbarItems = [
   {
@@ -32,5 +35,20 @@ export const HeroItems = [
     subtitle: "Come Watch Various Streaming & Thiller Movies and Many others",
     service:
       "Enjoy the various services we provide for streaming movies & watching thillers and you can also get others at a very cheap price",
+  },
+];
+
+export const SceneItems = [
+  {
+    id: 1,
+    img: scene1,
+  },
+  {
+    id: 2,
+    img: scene2,
+  },
+  {
+    id: 3,
+    img: scene3,
   },
 ];
