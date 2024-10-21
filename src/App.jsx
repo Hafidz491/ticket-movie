f2904fb2fd241cd231c73f3dc3f1c0a509732f41
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-10-22 06:10:34 +0700

    build section main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Heroes } from "./components/heroes/Heroes";
+import { MainSection } from "./components/mainSection/MainSection";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Navbar />
       <Heroes />
+      <MainSection />
     </div>
   );
 }
---
+++ src/components/mainSection/MainSection.jsx
@@ -0,0 +1,12 @@
+export const MainSection = () => {
+  return (
+    <div className="px-10 py-4">
+      <div className="flex items-center justify-between">
+        <h1>The Best Movie Recomendation & the Latest Movies from us</h1>
+        <h3>
+          Enjoy Various films that we have recommended for you and your familly
+        </h3>
+      </div>
+    </div>
+  );
+};
